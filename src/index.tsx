dbceb45f71810f5b8469bc1b0486112a2a9f2300
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-10-28 23:04:13 +0530

    Add a CI file to check format on push
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-import Header from './components/header/header';
+import Header from "./components/header/header";
 
 // UIKit import
-import 'uikit/dist/css/uikit.min.css';
+import "uikit/dist/css/uikit.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
