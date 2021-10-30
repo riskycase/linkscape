6c99c57be1b22210eee6c9774caa4b7767b2944f
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-10-30 07:35:50 +0530

    Add beginning signin UI and backend
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,20 +1,84 @@
 import Styles from "./header.module.scss";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithRedirect,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../../firebase";
+import { useState } from "react";
 
-function header() {
+const provider = new GoogleAuthProvider();
+
+function Header() {
+  const [user, setUser] = useState(auth.currentUser);
+  const [dropdownVisibility, setDropdownVisibility] = useState(false);
+  onAuthStateChanged(auth, (user) => {
+    setUser(user);
+    if (user === null) setDropdownVisibility(false);
+  });
   return (
     <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
-      <nav className="uk-navbar-container uk-padding-small" uk-navbar="true">
+      <nav
+        className={`uk-navbar-container uk-padding-small ${Styles.navbar}`}
+        uk-navbar="true"
+      >
         <div className={Styles.navItem}>
           <button
-            className="uk-button uk-button-link"
-            onClick={() => console.log("hoho")}
+            className={`uk-button uk-button-link ${Styles.signInButton}`}
+            onClick={() => signInWithRedirect(auth, provider)}
+            hidden={user !== null}
+            id="signInButton"
           >
             Sign in
           </button>
+          <div
+            hidden={user === null}
+            className={`uk-inline ${Styles.userContainer}`}
+          >
+            <div className={Styles.userDisplay}>
+              <img
+                src={user?.photoURL || ""}
+                id="userImage"
+                alt="Signed in"
+                className={Styles.userImage}
+              ></img>
+              <div
+                className={`${Styles.dropdown} ${
+                  dropdownVisibility ? Styles.flip : ""
+                }`}
+                onClick={() => setDropdownVisibility(!dropdownVisibility)}
+              />
+            </div>
+            <div
+              className={`${Styles.userOptions} ${
+                dropdownVisibility ? "" : Styles.hidden
+              }`}
+            >
+              <ul className={`uk-list uk-padding-small ${Styles.optionsList}`}>
+                <li>
+                  <button className="uk-button uk-button-link">
+                    {user?.displayName}
+                  </button>
+                </li>
+                <li>
+                  <button className="uk-button uk-button-link">My links</button>
+                </li>
+                <li>
+                  <button
+                    className="uk-button uk-button-link"
+                    onClick={() => signOut(auth)}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </nav>
     </div>
   );
 }
 
-export default header;
+export default Header;
