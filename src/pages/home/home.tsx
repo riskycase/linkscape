dfe7c977d1277ddbb9d104d8d0fff3181ad350c8
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-11-04 00:30:48 +0530

    Add a basic home page
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+import Styles from "./home.module.scss";
+
 function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <div className={Styles.homeContainer}>
+      <span className={Styles.homeHeading}>
+        Linkscape, a user powered hub which allows you to organise and share
+        academic related links with anyone
+      </span>
+      <div className={Styles.buttonGroup}>
+        <Link to="/links">
+          <button className="uk-button uk-button-primary uk-border-pill">
+            Explore links
+          </button>
+        </Link>
+        <Link to="/add">
+          <button className="uk-button uk-button-primary uk-border-pill">
+            Add a link
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
